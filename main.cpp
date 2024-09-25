42d9a335a172d427165aebb45e5df3bf6dc0b094
Kitz2024 <kit.pollinger@gmail.com>
2024-09-25 10:12:26 -0700

    Lab-15 Commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//Kit pollinger
+// 210 | Lab 9 | Movie Class
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main (){
+ // File Checker
+    ifstream inFile("C:/Lab210/lab15-input.txt"); //Change Path if placed somewhere else and not at "C:"
+    if (!inFile.is_open())
+    {
+        cout << "Error Can't open file" << endl;
+        return 1;
+    }
+
+return 0;
+}
