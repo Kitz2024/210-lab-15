b244516c489da0c53cf84b78d24682892ac6c63e
Kitz2024 <kit.pollinger@gmail.com>
2024-09-27 20:27:16 -0700

    9/27 Commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-//Kit pollinger
-// 210 | Lab 9 | Movie Class
+// Kit pollinger
+//  210 | Lab 9 | Movie Class
 
 #include <iostream>
 #include <string>
@@ -7,14 +7,59 @@
 #include <vector>
 using namespace std;
 
-int main (){
- // File Checker
-    ifstream inFile("C:/Lab210/lab15-input.txt"); //Change Path if placed somewhere else and not at "C:"
+class Movie
+{
+public:
+    Movie() {}
+    Movie(string title, int yearRealsed, string screenWriter)
+    {
+        this->title = title;
+        this->yearReleased = yearRealsed;
+        this->screenWriter = screenWriter;
+    }
+    string getTitle() const
+    {
+        return title;
+    }
+    void setTitle(string title)
+    {
+        this->title = title;
+    }
+
+private:
+    string title;
+    int yearReleased;
+    string screenWriter;
+};
+
+int main()
+{
+
+    vector<Movie> movies;
+    // File Checker
+    ifstream inFile("C:/Lab210/lab15-input.txt"); // Change Path if placed somewhere else and not at "C:"
     if (!inFile.is_open())
+    {
+        string title, screenWriter;
+        int yearReleased;
+
+        while (inFile >> title >> yearReleased >> screenWriter)
+        {
+            Movie movie(title, yearReleased, screenWriter);
+            movies.push_back(movie);
+        }
+        inFile.close();
+    }
+    else
     {
         cout << "Error Can't open file" << endl;
         return 1;
     }
 
-return 0;
+    cout << "Movies in the container: " << endl;
+    for (const Movie& movie : movies){
+        
+    }
+
+    return 0;
 }
