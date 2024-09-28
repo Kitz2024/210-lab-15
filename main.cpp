edffd0cac5f4968983a5e7099f1822af678130ac
Kitz2024 <kit.pollinger@gmail.com>
2024-09-27 22:15:41 -0700

    10PM Commit

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
-// Kit pollinger
-//  210 | Lab 9 | Movie Class
-
 #include <iostream>
+#include <vector>
 #include <string>
 #include <fstream>
-#include <vector>
+
 using namespace std;
 
 class Movie
 {
+private:
+    string title;
+    int yearReleased;
+    string screenwriter;
+
 public:
-    Movie() {}
-    Movie(string title, int yearRealsed, string screenWriter)
+    Movie()
+    {
+        title = "";
+        yearReleased = 0;
+        screenwriter = "";
+    }
+    Movie(string title, int yearReleased, string screenwriter)
     {
         this->title = title;
-        this->yearReleased = yearRealsed;
-        this->screenWriter = screenWriter;
+        this->yearReleased = yearReleased;
+        this->screenwriter = screenwriter;
     }
     string getTitle() const
     {
@@ -26,39 +34,56 @@ public:
         this->title = title;
     }
 
-private:
-    string title;
-    int yearReleased;
-    string screenWriter;
+    int getYearReleased() const
+    {
+        return yearReleased;
+    }
+    void setYearReleased(string screenwriter)
+    {
+        this->yearReleased = yearReleased;
+    }
+    string getScreenwriter() const
+    {
+        return screenwriter;
+    }
+    void setScreenwriter(string screenwriter)
+    {
+        this->screenwriter = screenwriter;
+    }
+    void print() const
+    {
+        cout << "Movie: " << title << endl;
+        cout << "\tYear released: " << yearReleased << endl;
+        cout << "\tScreenwriter: " << screenwriter << endl;
+    }
 };
 
 int main()
 {
-
     vector<Movie> movies;
-    // File Checker
-    ifstream inFile("C:/Lab210/lab15-input.txt"); // Change Path if placed somewhere else and not at "C:"
-    if (!inFile.is_open())
+    ifstream inFile("C:\\Lab210\\210lab15.txt");
+
+    if (inFile.is_open())
     {
-        string title, screenWriter;
+        string title, screenwriter;
         int yearReleased;
 
-        while (inFile >> title >> yearReleased >> screenWriter)
+        while (inFile >> title >> yearReleased >> screenwriter)
         {
-            Movie movie(title, yearReleased, screenWriter);
-            movies.push_back(movie);
+            Movie *movie = new Movie(title, yearReleased, screenwriter);
+            movies.push_back(*movie);
         }
         inFile.close();
     }
     else
     {
-        cout << "Error Can't open file" << endl;
+        cout << "Error opening File." << endl;
         return 1;
     }
-
-    cout << "Movies in the container: " << endl;
-    for (const Movie& movie : movies){
-        
+    for (const Movie &movie : movies)
+    {
+        cout << "Movies in File: " << endl;
+        movie.print();
     }
 
     return 0;
